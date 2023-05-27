081f0f46b5b1efb635db46b574ecfabec68ae970
Chang H Moon <changhakmoon1@gmail.com>
2023-05-27 13:53:19 -0400

    login register page

--- client/src/component/FillerPage.js
+++ client/src/component/FillerPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
 
 export default function Filler() {
   return (
     <div>
       <h1>Testing</h1>
+      <Link to="/register">Register Test Link</Link>
+      <Link to="/login">Login Test Link</Link>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,57 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-// const cors = require('cors');
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const cors = require('cors');
+const User = require('./models/User')
 
 // app.use(cors());
 
+const app = express();
 // Router
 //const apiRouter = require('./routes/api');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+//connect to monogoDB
+mongoose.connect(
+  `mongodb+srv://testingdb:testingdb@cluster0.gs1nz9c.mongodb.net/`
+);
+
 // Leads us to the route handlers
 //app.use('/', apiRouter);
+
+
+//route to /register
+app.post("/register", async (req,res)=>{
+  const {username, password} = req.body
+  try{
+    const userDoc = new User.create({
+      username,
+      password,
+    });
+    console.log("here---------" , userDoc)
+    res.json(userDoc)
+
+   }catch (err){
+    res.status(400).json(err.message)
+  }
+})
+
+app.post("/login", async (req, res)=>{
+  const {username, password} = req.body
+  try{
+    const userDoc = await User.find({username})
+    // const verified = //for bcrypt later
+
+    //need to fill this part.. cookies and jwt..
+
+  } catch(err){
+    res.status(400).json("wrong password or username")
+  }
+})
 
 // Global error handler
 app.use((err, _req, res, _next) => {
