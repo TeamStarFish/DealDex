847330d5fa1a9694998977288da584eabdc6db00
Patrick Vuong <patrickv77@gmail.com>
2023-05-27 12:29:07 -0500

    react skeleton

---
+++ client/src/component/ProductSpecifications.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Size, PriceMax, PriceMin, Resolution } from '../specs' //etc
+
+export default function ProductSpecifications() {
+  return (
+    <div>
+      <h1>productspecs! unpack them spec components</h1>
+    </div>
+  );
+}
---
+++ client/src/component/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SearchBar() {
+  return (
+    <div>
+      <h1>this the search bar</h1>
+    </div>
+  );
+}
