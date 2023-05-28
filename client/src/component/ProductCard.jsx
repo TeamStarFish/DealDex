6b4ddce39b294490411fa9ad55cfa73bda1551ba
Patrick Vuong <patrickv77@gmail.com>
2023-05-28 11:58:02 -0500

    notes on styling logic

--- client/src/component/ProductCard.jsx
+++ client/src/component/ProductCard.jsx
@@ -11,3 +11,15 @@ export default function ProductCard(props) {
     </div>
   );
 }
+
+//potentially bulky css incoming
+
+/*    IDEAS
+  NAME OF PRODUCT (MINUS WEIRD ASS TECH SPECS IN TITLE?)
+  card div holds an image, scaled down to fit the card
+  MAKER, sony etc
+  price, ordered from least to greatest
+  specs bolded, and printed either in line or if we stick to 4 specs, we can do 4 corners of a div?
+  space between for cards,
+  overflow y axis
+*/
--- client/src/component/ProductList.jsx
+++ client/src/component/ProductList.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ProductCard from './ProductCard.jsx';
 
 export default function ProductList(props) {
-  const { product } = props;
+  //const { product } = props;
+  const product = 'Monitor'
   let arr = ['size', 'pixels', 'girth', 'power', 'torque', 'acornsPerMinute'];
   return (
     <div className="flex flex-col border border-solid border-red-400 flex-grow">
       <p className="border border-solid border-black text-center">
-        product category, drilled from main TV
+        {product+'s'}
       </p>
-      <div className="border border-solid border-purple-400 flex-grow">
+      <div className="border border-solid border-purple-400 flex-grow overflow-y-auto">
         {arr.map((ele, i) => (
           <ProductCard key={i} spec={ele} />
         ))}
@@ -17,3 +18,5 @@ export default function ProductList(props) {
     </div>
   );
 }
+
+//in line 9-11 element, currently centered based on parent element size, maybe try to center based on total width? not sure how to do it tbh
