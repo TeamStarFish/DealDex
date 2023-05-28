7e02a8894c9d0b3a275ade97690ea6678f1b3420
Patrick Vuong <patrickv77@gmail.com>
2023-05-28 16:28:56 -0500

    flexbox on cards

--- client/src/component/ProductCard.jsx
+++ client/src/component/ProductCard.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 export default function ProductCard(props) {
+  //dummy props
+  const title = 'super awesome tv';
+  const imageURL =
+    'https://images.unsplash.com/photo-1541562232579-512a21360020?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80';
+  const price = '100000'
   const { spec } = props;
   return (
-    <div>
-      <h1>Product Card</h1>
+    <div className="border border-solid border-orange-400 ">
+      <h1>{title}</h1>
+      <img className="object-scale-down h-48 w-48"
+        src={imageURL}></img>
+      <p>price: {'$'+ price}</p>
       <div>
         <p>{spec}</p>
       </div>
@@ -22,4 +30,14 @@ export default function ProductCard(props) {
   specs bolded, and printed either in line or if we stick to 4 specs, we can do 4 corners of a div?
   space between for cards,
   overflow y axis
+
+  specs = [{"spec": "specValue"}, {"spec2: specValue2"}]
+  or
+  specs = [["spec": "specValue"], ["spec2: specValue2"]]
+  or
+  specs = {
+    spec1: spec1value
+    spec2: spec2value
+    etc
+  }
 */
--- client/src/component/ProductList.jsx
+++ client/src/component/ProductList.jsx
@@ -6,11 +6,11 @@ export default function ProductList(props) {
   const product = 'Monitor'
   let arr = ['size', 'pixels', 'girth', 'power', 'torque', 'acornsPerMinute'];
   return (
-    <div className="flex flex-col border border-solid border-red-400 flex-grow">
+    <div className="flex flex-col border border-solid border-red-400 flex-grow overflow-y-auto">
       <p className="border border-solid border-black text-center">
         {product+'s'}
       </p>
-      <div className="border border-solid border-purple-400 flex-grow overflow-y-auto">
+      <div className="flex border-2 border-solid border-purple-400 flex-grow justify-start items-start gap-16 m-4  flex-wrap">
         {arr.map((ele, i) => (
           <ProductCard key={i} spec={ele} />
         ))}
