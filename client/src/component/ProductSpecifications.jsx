9f166d4e9b58e11cc7b5353e74da82fee72a7c84
Cortland Young <dcy.y@outlook.com>
2023-05-28 19:50:37 -0500

    added options to search bar; added search button to search bar; need to implement onChange for search button and filter product list based on product specs.

--- client/src/component/ProductSpecifications.jsx
+++ client/src/component/ProductSpecifications.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { Size, PriceMax, PriceMin, Resolution } from './Specs.jsx' //add spec to Specs.jsx and import here to use in search filter
+import PriceMax from './Specs/PriceMax.jsx';
+import PriceMin from './Specs/PriceMin.jsx';
+import Size from './Specs/Size.jsx';
+import Resolution from './Specs/Resolution.jsx';
+import RefreshRate from './Specs/RefreshRate.jsx';
+
 
 export default function ProductSpecifications(props) {
+  // destructure product selection specs from props
+  const {monitorSpecs, tvSpecs} = props;
+
   return (
     <div>
       <h1>productspecs! unpack them spec components</h1>
+      {monitorSpecs && (
+        <div>
+          <h2>Monitor Specs </h2>
+          <PriceMax></PriceMax>
+          <PriceMin></PriceMin>
+          <Size></Size>
+          <Resolution monitorSpecs></Resolution>
+          <RefreshRate monitorSpecs></RefreshRate>
+          </div>
+      )}
+      {tvSpecs && (
+        <div>
+          <h2>TV Specs </h2>
+          <PriceMax></PriceMax>
+          <PriceMin></PriceMin>
+          <Size></Size>
+          <Resolution tvSpecs></Resolution>
+          </div>
+      )}
     </div>
   );
 }
