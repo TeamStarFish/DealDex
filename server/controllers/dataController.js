6dd273c2e2768fa3f370cccc5b90e5b3211d04bd
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-05-30 17:21:22 -0700

    Removed unnecessary dependencies

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -1,6 +1,5 @@
 const { query } = require('express');
 const db = require('../models/itemModel');
-const axios = require('axios');
 
 // set up the request parameters
 
@@ -14,16 +13,15 @@ dataController.fetchProducts = async (req, res, next) => {
   SELECT * FROM products
   WHERE category = '${category}'`;
 
-
   try {
     const response = await db.query(queryStr);
     res.locals.products = response.rows;
     return next();
   } catch (err) {
-    return next({ 
-        log: 'fetchProducts ',
-        status: 400,
-        message: `Error in returning fetched Product, ${err}`
+    return next({
+      log: 'fetchProducts ',
+      status: 400,
+      message: `Error in returning fetched Product, ${err}`,
     });
   }
 };
