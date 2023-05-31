a524fa954da096709e8dd9677784cfebe6de5e5b
Alan Beck <kalanbeck@Skunk>
2023-05-31 14:47:56 -0400

    built product api

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,29 +1,50 @@
 const { query } = require('express');
 const db = require('../models/itemModel');
-
-// set up the request parameters
+const data = require("./TEST_DATA");
 
 const productController = {};
 
 productController.fetchProducts = async (req, res, next) => {
-  console.log('hello');
-  const { category } = req.body;
+  const search = req.body.search.replace(/\s/g, "%20");
 
-  const queryStr = `
-  SELECT * FROM products
-  WHERE category = '${category}'`;
+  const url = `https://real-time-product-search.p.rapidapi.com/search?q=${search}&country=us&language=en&sort_by=TOP_RATED`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '3f1385b474msh5cc468c6c6ca770p1784a6jsne23de33e28ca',
+      'X-RapidAPI-Host': 'real-time-product-search.p.rapidapi.com'
+    }
+  };
 
   try {
-    const response = await db.query(queryStr);
-    res.locals.products = response.rows;
-    return next();
-  } catch (err) {
-    return next({
-      log: 'fetchProducts ',
-      status: 400,
-      message: `Error in returning fetched Product, ${err}`,
-    });
+    const response = await fetch(url, options);
+    const result = await response.text();
+    // console.log(result);
+
+    res.locals.products = result.data.map((product) => {
+      const { product_title, product_photos } = product;
+      const { store_name, offer_page_url, price, product_condition } = product.offer;
+      return {
+        product_title, 
+        product_photos,
+        store_name, 
+        offer_page_url, 
+        price, 
+        product_condition
+      }
+    })
+
+    next();
+  } catch (error) {
+    console.error(error);
   }
-};
+}
+
+// product_title
+// product_photos[0]
+// offer.store_name
+// offer.offer_page_url
+// offer.price
+// offer.product_condition
 
 module.exports = productController;
