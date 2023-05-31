401113d0533a1f7a418b85178fb79f805516d016
Kasualkid12 <kasu@kasu-it.dev>
2023-05-31 16:33:49 -0500

    add findRelevant middleware to sort through scraped html

--- server/controllers/scraperController.js
+++ server/controllers/scraperController.js
@@ -1,14 +1,18 @@
-// const { JSDOM } = require('jsdom');
+const { JSDOM } = require('jsdom');
 
 const scraperController = {};
 
-const url =
-  'https://www.amazon.com/Sony-PlayStation-Pro-1TB-Console-4/dp/B07K14XKZH/';
+const url = 'https://www.amazon.com/s?k=monitors&qid=1685563321&ref=sr_pg_1';
+const header = new Headers({
+  'User-Agent': 'My User Agent',
+});
 
 scraperController.scrape = async (req, res, next) => {
   try {
-    const resp = await fetch(url);
-    console.log((res.locals.html = await resp.text()));
+    const resp = await fetch(url, {
+      headers: header,
+    });
+    res.locals.html = await resp.text();
     return next();
   } catch (err) {
     return next({
@@ -20,6 +24,35 @@ scraperController.scrape = async (req, res, next) => {
   }
 };
 
-// const { document } = new JSDOM()
+scraperController.findRelevant = (req, res, next) => {
+  try {
+    const dom = new JSDOM(res.locals.html);
+    const document = dom.window.document;
+    const items = document.querySelectorAll(
+      'div.sg-col-20-of-24.s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.s-widget-spacing-small.sg-col-12-of-16'
+    );
+
+    res.locals.products = [];
+
+    for (let i = 0; i < items.length; i++) {
+      res.locals.products.push({
+        title: items[i].querySelector(
+          'span.a-size-medium.a-color-base.a-text-normal'
+        ).textContent,
+        price: items[i].querySelector('span.a-offscreen').textContent,
+        image: items[i].querySelector('img.s-image').src,
+      });
+    }
+
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in scraperController.findRelevant: ${err}`,
+      message: {
+        err: 'An error occured, check server logs',
+      },
+    });
+  }
+};
 
 module.exports = scraperController;
