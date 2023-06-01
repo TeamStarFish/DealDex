f85b015d768e40f7f0457ef70c6b91050113a455
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-05-31 18:00:05 -0700

    Displays scraped content

--- client/src/component/ProductCard.jsx
+++ client/src/component/ProductCard.jsx
@@ -1,46 +1,39 @@
 import React from 'react';
+// import ProductCardSpecs from './ProductCardSpecs.jsx';
 
 export default function ProductCard(props) {
   //dummy url for now
   // const imageURL =
   //   'https://images.unsplash.com/photo-1541562232579-512a21360020?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80';
   const { name, price, specs, imageURL } = props;
+
   // creating key array so we can use map to render the product card specs
   // these show up underneath the product card name, image, and price
   // this is not an ideal method but due for MVP purposes it's what we've got lol
-  let keyArr = [];
-  let results = [];
-  if (specs) {
-    keyArr = Object.keys(specs);
-    // filter out the name, price, and category since these are already displayed
-    results = keyArr.filter(
-      (ele) =>
-        ele !== 'img' &&
-        ele !== 'category' &&
-        ele !== 'price' &&
-        ele !== 'productname'
-    );
-  }
+  // let keyArr = [];
+  // let results = [];
+  // if(specs){
+  //   keyArr = Object.keys(specs);
+  //   // filter out the name, price, and category since these are already displayed
+  //   results = keyArr.filter(ele => ele !== 'img' && ele !== 'category' && ele !== 'price' && ele !== 'productname');
+  // }
 
   return (
     <div className="h-100 w-100">
-      <h1 className="text-center text-[24px] overflow-hidden line-clamp-2 hover:line-clamp-none">
-        {name}
-      </h1>
+      <h1 className="text-center text-[24px]">{name}</h1>
       <img
         className="object-scale-down h-64 w-64 block mx-auto"
         src={imageURL}
       ></img>
       <p className="text-center text-[20px]">{'$' + price}</p>
-      <div className="grid grid-cols-3 m-4">
-        {results.map((ele, i) => (
-          <div className="m-1">
-            <p className="text-center">
-              <strong>{ele}</strong>
-            </p>
-            <p className="text-center">{specs[ele]}</p>
-          </div>
-        ))}
+      <div className="grid grid-cols-2 m-4">
+        {/* {results.map((ele, i) => (
+          <ProductCardSpecs
+            key={results[i]+i} 
+            spec={ele}
+            specVal={specs[ele]}
+          />
+        ))} */}
       </div>
     </div>
   );
--- client/src/component/ProductList.jsx
+++ client/src/component/ProductList.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ProductCard from "./ProductCard.jsx";
+import React from 'react';
+import ProductCard from './ProductCard.jsx';
 
 export default function ProductList(props) {
   const { product, specs } = props;
 
   return (
-    <div className='flex flex-col border border-solid border-red-400 flex-grow items-stretch '>
-      <p className='border border-solid border-black text-center'>{product}</p>
-      <div className='h-20 grid grid-cols-3 border overflow-y-scroll border-solid border-purple-400 flex-grow justify-start items-start gap-8 m-4 flex-wrap'>
+    <div className="flex flex-col border border-solid border-red-400 flex-grow items-stretch ">
+      <p className="border border-solid border-black text-center">{product}</p>
+      <div className="h-20 grid grid-cols-3 border overflow-y-scroll border-solid border-purple-400 flex-grow justify-start items-start gap-8 m-4 flex-wrap">
         {specs.map((ele, i) => (
           <ProductCard
-            key={"prodcard" + i}
-            name={ele.productname}
+            key={'prodcard' + i}
+            name={ele.title}
             price={ele.price}
-            imageURL={ele.img}
-            specs={ele}
+            imageURL={ele.image}
+            // specs={ele}
           />
         ))}
       </div>
